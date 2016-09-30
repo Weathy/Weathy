56bbce5faf014756ee0fffc6cac89ea1b64b9b31
Martin Andonov <martin.l.andonov@abv.bg>
2016-09-30 17:51:10 +0300

    Reverted

--- src/main/java/com/example/kali/weathy/WeatherActivity.java
+++ src/main/java/com/example/kali/weathy/WeatherActivity.java
@@ -1,6 +1,6 @@
 package com.example.kali.weathy;
 
-import android.app.ActionBar;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,17 +12,29 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.kali.weathy.model.Weather;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class WeatherActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, TenDayFragment.TenDayComunicator,
         TwentyFourFragment.TwentyFourComunicator, CityForecastFragment.CityForecastComunicator {
-
-    private Button searchButton;
 
     private WeatherPagerAdapter adapter;
 
@@ -31,21 +43,18 @@ public class WeatherActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
+        new RequestTask().execute();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         ViewPager vPager = (ViewPager) findViewById(R.id.view_pager);
         adapter = new WeatherPagerAdapter(getSupportFragmentManager());
         vPager.setAdapter(adapter);
-
     }
 
 
@@ -76,7 +85,6 @@ public class WeatherActivity extends AppCompatActivity
         public int getCount() {
             return NUMBER_OF_FRAGMENTS;
         }
-
     }
 
     @Override
@@ -136,6 +144,73 @@ public class WeatherActivity extends AppCompatActivity
         return true;
     }
 
-    public void changeFragment(Fragment f){
+    public void changeFragment(Fragment f) {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction trans = fm.beginTransaction();
+        trans.replace(R.id.view_pager, new SearchFragment());
+        adapter.notifyDataSetChanged();
+        trans.commit();
+
+    }
+
+    class RequestTask extends AsyncTask<Void, Void, Weather> {
+        StringBuilder coordinatesJSON = new StringBuilder();
+        StringBuilder weatherJSON = new StringBuilder();
+        String lat;
+        String lng;
+        String cityName;
+        @Override
+        protected Weather doInBackground(Void... params) {
+            try {
+                URL coordinates = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=София&key=AIzaSyCmuqUqYeQXBzTT4qpKPBI5uhSLW1m-l2I");
+                Log.e("url", coordinates.toString());
+                HttpURLConnection connection = (HttpURLConnection) coordinates.openConnection();
+                connection.setRequestMethod("GET");
+                InputStream stream = connection.getInputStream();
+                Scanner sc = new Scanner(stream);
+                while (sc.hasNextLine()) {
+                    coordinatesJSON.append(sc.nextLine());
+                }
+                JSONObject weatherObj = new JSONObject(coordinatesJSON.toString());
+                JSONArray array = weatherObj.getJSONArray("results");
+                JSONObject resultInArr = array.getJSONObject(0);
+                cityName = resultInArr.getString("formatted_address");
+                lat = resultInArr.getJSONObject("geometry").getJSONObject("location").getString("lat");
+                lng = resultInArr.getJSONObject("geometry").getJSONObject("location").getString("lng");
+                sc.close();
+                stream.close();
+
+                URL weatherInfo = new URL("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lng+"&appid=9d01db38e0b771b0eb2fffa9e3640dd9");
+                HttpURLConnection weatherConnection = (HttpURLConnection) weatherInfo.openConnection();
+                weatherConnection.setRequestMethod("GET");
+                InputStream weatherStream = weatherConnection.getInputStream();
+                Scanner weatherScanner = new Scanner(weatherStream);
+                while (weatherScanner.hasNextLine()) {
+                    weatherJSON.append(weatherScanner.nextLine());
+                }
+                JSONObject weather = new JSONObject(weatherJSON.toString());
+                //cityName = weather.getString("name");
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return new Weather(cityName,0,0,0,null,null,null,null,0,0,0,0);
+        }
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            Log.e("weather", weatherJSON.toString());
+            Log.e("weather", "lat:" + lat + " lng" + lng);
+            if(weather!=null)
+                Toast.makeText(WeatherActivity.this, weather.getCityName() , Toast.LENGTH_SHORT).show();
+            else{
+                Toast.makeText(WeatherActivity.this, "no weather", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- src/main/java/com/example/kali/weathy/model/Weather.java
+++ src/main/java/com/example/kali/weathy/model/Weather.java
@@ -1,8 +1,83 @@
-package com.example.pc.weathy.model;
+package com.example.kali.weathy.model;
 
 /**
  * Created by Kali on 28.9.2016 г..
  */
 
 public class Weather {
+    private  String cityName;
+    private String description;
+    private String icon;
+    private int temp_min;
+    private int temp_max;
+    private double latitude;
+    private double longitude;
+    private String sunrise;
+    private String sunset;
+    private double windSpeed;
+    private int humidity;
+    private int pressure;
+
+    public Weather(String cityName, int humidity, double windSpeed, int pressure, String sunrise, String sunset, String description, String icon, int temp_min, int temp_max, double latitude, double longitude) {
+        this.cityName = cityName;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+        this.pressure = pressure;
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+        this.description = description;
+        this.icon = icon;
+        this.temp_min = temp_min;
+        this.temp_max = temp_max;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public int getTemp_min() {
+        return temp_min;
+    }
+
+    public int getTemp_max() {
+        return temp_max;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
 }
