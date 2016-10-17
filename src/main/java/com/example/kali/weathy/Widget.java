9e8f944be1a6d8ef4494dca251229353216a9e27
Martin Andonov <martin.l.andonov@abv.bg>
2016-10-17 18:20:30 +0300

    Widget icon

--- src/main/java/com/example/kali/weathy/Widget.java
+++ src/main/java/com/example/kali/weathy/Widget.java
@@ -5,9 +5,12 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 import com.example.kali.weathy.database.DBManager;
+
+import java.util.Calendar;
 
 public class Widget extends AppWidgetProvider {
 
@@ -23,6 +26,7 @@ public class Widget extends AppWidgetProvider {
             views.setTextViewText(R.id.widnget_current_temp_tv, DBManager.getInstance(context).getLastWeather().getCurrentTemp()+"℃");
             views.setTextViewText(R.id.widget_city_name_tv, DBManager.getInstance(context).getLastWeather().getCityName()+"");
             views.setTextViewText(R.id.widget_condition_tv, DBManager.getInstance(context).getLastWeather().getDescription()+"");
+            views.setImageViewResource(R.id.widget_icon_imageview, setIcon(DBManager.getInstance(context).getLastWeather().getDescription()));
 
             if(DBManager.getInstance(context).getTenDayForecast().size() != 0) {
                 views.setTextViewText(R.id.widget_min_temp_tv, DBManager.getInstance(context).getTenDayForecast().get(0).getMinTemp() + "℃");
@@ -34,7 +38,51 @@ public class Widget extends AppWidgetProvider {
             views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
             appWidgetManager.updateAppWidget(currentWidgetId,views);
+        }
+    }
 
+    private int setIcon(String condition){
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+
+        if (hour > 7 && hour < 20) {
+            switch (condition) {
+                case "Clear":
+                    return R.drawable.big_clear_day;
+                case "Clouds":
+                    return R.drawable.big_cloudy;
+                case "Drizzle":
+                    return R.drawable.big_rain_day;
+                case "Rain":
+                    return R.drawable.big_rain_day;
+                case "Thunderstorm":
+                     return R.drawable.big_thunderstorm;
+                case "Snow":
+                    return R.drawable.big_snow;
+                case "Atmosphere":
+                    return R.drawable.big_atm;
+                default:
+                    return R.drawable.big_not_available;
+            }
+        } else {
+            switch (condition) {
+                case "Clear":
+                    return R.drawable.big_clear_night;
+                case "Clouds":
+                    return R.drawable.big_cloudy;
+                case "Drizzle":
+                    return R.drawable.big_rain_night;
+                case "Rain":
+                    return R.drawable.big_rain_night;
+                case "Thunderstorm":
+                    return R.drawable.thunderstorm_night;
+                case "Snow":
+                    return R.drawable.big_snow;
+                case "Atmosphere":
+                    return R.drawable.big_atm;
+                default:
+                    return R.drawable.big_not_available;
+            }
         }
     }
 
